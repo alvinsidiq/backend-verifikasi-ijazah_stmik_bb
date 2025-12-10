120dfb0079d6e372eb9ae9ab95ab7143c72dd532
alvin sidiq <alvinsidiq2001@gmail.com>
2025-12-10 21:13:50 +0800

    tambah  user registration endpoint untuk  admin

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,12 +2,20 @@
 const express = require('express');
 const AuthController = require('../controllers/auth.controller');
 const authMiddleware = require('../middlewares/authMiddleware');
-
+const roleMiddleware = require('../middlewares/roleMiddleware');
 const router = express.Router();
 
 // POST /auth/login
 router.post('/login', AuthController.login);
 
+
+// POST /auth/register  (hanya ADMIN yang sudah login)
+router.post(
+  '/register',
+  authMiddleware,
+  roleMiddleware(['ADMIN']),
+  AuthController.register
+);
 
 // GET /auth/me (butuh token)
 router.get('/me', authMiddleware, AuthController.me);
